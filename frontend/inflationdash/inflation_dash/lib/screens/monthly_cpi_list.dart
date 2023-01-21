63897289081058af663d95da47a7d17c29bf3a42
Arjun Arun <arjunmjarun@gmail.com>
2023-01-21 10:33:12 -0800

    Cleaning up the line chart, re-titling X axis, etc.

--- frontend/inflationdash/inflation_dash/lib/screens/monthly_cpi_list.dart
+++ frontend/inflationdash/inflation_dash/lib/screens/monthly_cpi_list.dart
@@ -25,8 +25,8 @@ class _MonthlyCPIListState extends State<MonthlyCPIList> {
         Iterable list = json.decode(response.body);
         print(list);
         monthlyCPIList = list.map((model) => InflationPercentage.fromJson(model)).toList();
-        cpi_only_list = monthlyCPIList.map((x) => x.inflation_percentage * 100).toList();
-        for (int i = 0; i <= 5; i++) {
+        cpi_only_list = monthlyCPIList.map((x) => double.parse((x.inflation_percentage * 100).toStringAsFixed(1))).toList();
+        for (int i = 0; i <= 9; i++) {
           final_list.add(FlSpot(x_axis_points[i], cpi_only_list[i]));
         }
       });
@@ -50,11 +50,39 @@ class _MonthlyCPIListState extends State<MonthlyCPIList> {
           height: 300,
           child: LineChart(
             LineChartData(
-              borderData: FlBorderData(show: false),
+              minY: 2,
+              maxY: 9,
+              borderData: FlBorderData(  
+                show: true,
+                border: Border.all(color: const Color(0xff37434d)),
+              ),
+              gridData: FlGridData(
+                show: true,
+                drawHorizontalLine: true,
+                drawVerticalLine: false,
+                verticalInterval: 1,
+                horizontalInterval: 1
+              ),
               titlesData: FlTitlesData(
                 show: true,
+                rightTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false)
+                ),
                 topTitles: AxisTitles(
-                  axisNameWidget: Text("Monthly Inflation Percentage")
+                  axisNameWidget: Text(
+                    "Monthly Inflation Percentage",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ) 
+                  )
+                ),
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 30,
+                    interval: 1,
+                    getTitlesWidget: bottomTitleWidgets,
+                  )
                 )
               ),
               lineBarsData: [
@@ -66,4 +94,59 @@ class _MonthlyCPIListState extends State<MonthlyCPIList> {
           )
       ));
   }
+}
+
+Widget bottomTitleWidgets(double value, TitleMeta meta) {
+  const style = TextStyle(
+    color: Color(0xff68737d),
+    fontWeight: FontWeight.bold,
+    fontSize: 12
+  );
+  Widget text;
+  switch (value.toInt()) {
+    case 1:
+      text = const Text('JAN', style:style);
+      break;
+    case 2:
+      text = const Text('FEB', style:style);
+      break;
+    case 3:
+      text = const Text('MAR', style:style);
+      break;
+    case 4:
+      text = const Text('APRIL', style:style);
+      break;
+    case 5:
+      text = const Text('MAY', style:style);
+      break;
+    case 6:
+      text = const Text('JUNE', style:style);
+      break;
+    case 7:
+      text = const Text('JULY', style:style);
+      break;
+    case 8:
+      text = const Text('AUG', style:style);
+      break;
+    case 9:
+      text = const Text('SEPT', style:style);
+      break;
+    case 10:
+      text = const Text('OCT', style:style);
+      break;
+    case 11:
+      text = const Text('NOV', style:style);
+      break;
+    case 12:
+      text = const Text('DEC', style:style);
+      break;
+    default:
+      text = const Text('', style: style);
+      break;
+  }
+
+  return SideTitleWidget(
+    axisSide: meta.axisSide,
+    child: text,
+  );
 }
